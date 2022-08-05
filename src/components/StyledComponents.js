04eacf314d0b30cd4b237b5851189b6cf83e1128
cojacgose <cojacgose@gmail.com>
2022-08-05 16:42:46 -0700

    Updates

--- src/components/StyledComponents.js
+++
@@ -1,6 +0,0 @@
-import UserContext from "../contexts/UserContext";
-import { ThemeContext } from "styled-components";
-import styled from "styled-components";
-
-export const CoolButton=styled.button`
-`
--- src/components/checkout/FinalOrderList.js
+++
@@ -1,9 +0,0 @@
-
-export default function FinalOrderList(props){
-    console.log("final order:",props.finalOrder)
-    return(
-        <>
-            <h1>{props.finaOrder}</h1>
-        </>
-    )
-}
